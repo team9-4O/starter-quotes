be609828fdcb1d64e4894ced93d241d99aa5f790
imoongom <moongom.ca@gmail.com>
2016-02-02 17:59:18 -0800

    Merge branch 'develop' of https://github.com/team9-4O/starter-quotes into moon
    # Conflicts:
    #	readme.rst

---
+++ application/controllers/First.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Our homepage. Show a table of all the author pictures. Clicking on one should show their quote.
+ * Our quotes model has been autoloaded, because we use it everywhere.
+ * 
+ * controllers/Welcome.php
+ *
+ * ------------------------------------------------------------------------
+ */
+class First extends Application {
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	//-------------------------------------------------------------
+	//  The normal pages
+	//-------------------------------------------------------------
+
+	function index()
+	{
+		$this->data['pagebody'] = 'justone';	// this is the view we want shown
+		// build the list of authors, to pass on to our view
+		$source = $this->quotes->all();
+		$authors = array();
+                $record = $this->quotes->get(1);
+                $this->data = array_merge($this->data, $record);
+		$this->data['authors'] = $authors;
+
+		$this->render();
+	}
+
+}
+
+/* End of file Welcome.php */
+/* Location: application/controllers/Welcome.php */
